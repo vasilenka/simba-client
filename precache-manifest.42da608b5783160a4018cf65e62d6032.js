51f137ce902d159d165ac9513341dce41a8b83b4
Ongki Herlambang <meepomodeon@gmail.com>
2019-03-02 11:34:49 +0700

    Updates

---
+++ precache-manifest.42da608b5783160a4018cf65e62d6032.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "80c098c1492949a1eaa1",
+    "url": "/simba-client/static/css/main.43ec7732.chunk.css"
+  },
+  {
+    "revision": "80c098c1492949a1eaa1",
+    "url": "/simba-client/static/js/main.80c098c1.chunk.js"
+  },
+  {
+    "revision": "7c2c90597efa3bf6a533",
+    "url": "/simba-client/static/js/runtime~main.7c2c9059.js"
+  },
+  {
+    "revision": "6113f9c03d08d78fb488",
+    "url": "/simba-client/static/js/2.6113f9c0.chunk.js"
+  },
+  {
+    "revision": "9201ddc05f7af3e6289c9c40860e67a2",
+    "url": "/simba-client/static/media/mid.9201ddc0.svg"
+  },
+  {
+    "revision": "c334c442cd72b7e2982c1364d9d75264",
+    "url": "/simba-client/static/media/github.c334c442.svg"
+  },
+  {
+    "revision": "adf46aa104c4f08a4b37de35d8157769",
+    "url": "/simba-client/static/media/chevron-right.adf46aa1.svg"
+  },
+  {
+    "revision": "3e63b8007dace1558794ee0198052a91",
+    "url": "/simba-client/index.html"
+  }
+];
