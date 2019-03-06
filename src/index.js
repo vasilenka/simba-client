c607be355dbca095a9427b28b0caac3ff5c2a403
Ongki Herlambang <meepomodeon@gmail.com>
2019-03-07 06:36:59 +0700

    update url for socket

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import * as io from 'socket.io-client'
 import SocketContext from './app/context/SocketContext'
 
-const socket = io(`${process.env.REACT_APP_SOCKET}`, {
+const socket = io(`${process.env.REACT_APP_WEB_HOST}`, {
   path: '/live'
 })
 
