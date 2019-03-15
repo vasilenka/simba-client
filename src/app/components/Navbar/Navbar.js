f93469b888388629e53a16256c292529e3550628
Ongki Herlambang <meepomodeon@gmail.com>
2019-03-15 11:27:28 +0700

    add small broadcast

--- src/app/components/Navbar/Navbar.js
+++ src/app/components/Navbar/Navbar.js
@@ -2,13 +2,44 @@ import styles from './Navbar.module.scss';
 import React from 'react';
 import cx from 'classnames';
 import {NavLink} from 'react-router-dom'
+import axios from 'axios'
 
 // import { ReactComponent as Logo } from './mid.svg';
 import dlogo from './dlogo.png'
 import Text from '../Text/Text';
 import Image from '../Image/Image';
+import Button from '../Button/Button';
+import Dialog from '../Dialog/Dialog';
+import Container from '../../layouts/Container/Container';
+import Textfield from '../Textfield/Textfield';
+import Luna from '../Luna/Luna';
+import SinglePreview from '../SinglePreview/SinglePreview';
+import FieldInput from '../FieldInput/FieldInput';
 
 const Navbar = ({ className, ...restProps }) => {
+
+  const postBroadcast = (title, body) => {
+    axios.post(`${process.env.REACT_APP_WEB_HOST}/broadcast`,
+      {
+        title: title,
+        body: body
+      }
+    )
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+  }
+
+  let [showDialog, setShowDialog] = React.useState()
+
+  let [title, setTitle] = React.useState("")
+  let [body, setBody] = React.useState("")
+
+  const sendBroadcast = () => {
+    if(title && body) {
+      postBroadcast(title, body)
+    }
+  }
+
   return (
     <div className={cx(styles.root)}>
       <div className={cx(styles.container)}>
@@ -51,6 +82,36 @@ const Navbar = ({ className, ...restProps }) => {
           </div>
         </div>
         <div className={styles.secondary}>
+          <Button small primary onClick={() => setShowDialog(true)}>New broadcast</Button>
+          {showDialog &&
+            <Dialog>
+              <Container narrow className={styles.broadcastView}>
+                <div style={{maxWidth: '560px'}}>
+                  <Button secondary small onClick={() => setShowDialog(false)} style={{marginBottom: '64px'}}>Close X</Button>
+                  <Text heading2 component="h1">New broadcast message</Text>
+                  <br/>
+                  <Text heading6 component="label" style={{display: 'block', marginBottom: '12px'}}>Judul broadcast</Text>
+                  <FieldInput
+                    id="title"
+                    name="title"
+                    value={title}
+                    setValue={v => setTitle(v)}
+                    />
+                  <br/>
+                  <Text heading6 component="label" style={{display: 'block', marginBottom: '12px'}}>Judul broadcast</Text>
+                  <FieldInput
+                    id="body"
+                    name="body"
+                    value={body}
+                    setValue={v => setBody(v)}
+                    />
+                  <br/>
+                  <br/>
+                </div>
+                <Button primary small onClick={sendBroadcast}>Send broadcast message</Button>
+              </Container>
+            </Dialog>
+          }
         </div>
       </div>
     </div>
